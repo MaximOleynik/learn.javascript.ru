fa1585d71a246c3bb72ea989ce1d6d19d2a656a7
MaximOleynik <oleynik.m.v@yandex.ru>
2022-04-09 23:25:19 +0600

    Loops: while and for

---
+++ PART 1. The JavaScript language/2. JavaScript Fundamentals/13. Loops - while and for/6.js
@@ -0,0 +1,5 @@
+let num;
+do {
+ num = prompt("Введите число");
+ if (num == null) break;
+} while (+num <= 100)
---
+++ PART 1. The JavaScript language/2. JavaScript Fundamentals/13. Loops - while and for/7.js
@@ -0,0 +1,7 @@
+let n = prompt("");
+outer: for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue outer;
+    }
+    alert(i);
+}
