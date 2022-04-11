61734de2fb40a932b997a9234a9269e0b3f74e7a
MaximOleynik <oleynik.m.v@yandex.ru>
2022-04-11 18:55:16 +0600

    Functions

---
+++ PART 1. The JavaScript language/2. JavaScript Fundamentals/15. Functions/4.js
@@ -0,0 +1,16 @@
+let x = prompt("x?");
+let n = prompt("n?");
+
+function pow(x, n) {
+    let result = x;
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+  } else {
+    alert( pow(x, n) );
+  }
